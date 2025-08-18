f3b1e35f32509fdcb700dbf330b4a4349338a9c8
Richard Massimilla <richiesmassi@gmail.com>
2025-08-18 13:24:21 -0400

    Add initial project files

---
+++ manim-test/main.py
@@ -0,0 +1,135 @@
+from manim import *
+
+
+class DefaultTemplate(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set color and transparency
+
+        square = Square()  # create a square
+        square.flip(RIGHT)  # flip horizontally
+        square.rotate(-3 * TAU / 8)  # rotate a certain amount
+
+        self.play(Create(square))  # animate the creation of the square
+        self.play(Transform(square, circle))  # interpolate the square into the circle
+        self.play(FadeOut(square))  # fade out animation
+
+class QuadraticFormula(Scene):
+    def construct(self):
+        formula = Tex(
+            "a", "x^2", "+b", "x", "+c", "=", "0",
+            arg_separator=" ",
+            tex_environment="align*"
+        )
+        subC = Tex(
+            "a", "x^2", "+b", "x", "=", "-c",
+            arg_separator=" ",
+            tex_environment="align*"
+        )
+        divA = Tex(
+            "x^2", "+{b", "\\over \\,", "a}", "x", "=", "-{c", "\\over \\,", "a}",
+            arg_separator=" ",
+            tex_environment="align*"
+        )
+
+        subC.move_to(formula)
+        divA.move_to(subC, UP + LEFT)
+
+        #print("formula ", [formula.get_part_by_tex(tex) for tex in ("a", "x^2", "b", "x", "=", "c")], "\n", 
+        #      "subC ", [subC.get_part_by_tex(tex) for tex in ("a", "x^2", "b", "x", "=", "c")])
+
+        self.play(Write(formula))  # animate the writing of the formula
+
+        self.wait(1)  # wait for a moment to let the formula appear
+
+        self.play(*[
+            ReplacementTransform(
+                formula.get_part_by_tex(tex),
+                subC.get_part_by_tex(tex),
+                run_time = 2,
+            )
+            for tex in ("a", "x^2", "b", "=", "c")
+        ] + [
+            ReplacementTransform(
+                formula.get_part_by_tex("x", substring=False),
+                subC.get_part_by_tex("x", substring=False),
+                run_time = 2,
+            )
+        ] + [
+            FadeOut(formula.get_part_by_tex("0"))
+        ])
+
+        self.wait(1)  # wait for a moment to let the transformation complete
+
+        self.play(*[
+            ReplacementTransform(
+                subC.get_part_by_tex(tex),
+                divA.get_part_by_tex(tex),
+                run_time = 2,
+            )
+            for tex in ("x^2", "b", "=", "c")
+        ] + [
+            ReplacementTransform(
+                subC.get_part_by_tex("x", substring=False),
+                divA.get_part_by_tex("x", substring=False),
+                run_time = 2,
+            )
+        ] + [
+            ReplacementTransform(
+                subC.get_part_by_tex("a"),
+                divA.get_parts_by_tex("a"),
+                run_time = 2,
+            )
+        ] + [
+            Write(divA.get_parts_by_tex("over"))
+        ])
+
+        self.wait(1)  # wait for a moment to let the transformation complete
+
+        square = Square(side_length=0.5)
+        linear = Rectangle(height=0.5, width=0.25)
+        const = Rectangle(height=0.75, width=0.5)
+        plus = Tex("+")
+        equal = Tex("=")
+        geoEq = VGroup(square, plus, linear, equal, const).arrange(RIGHT, buff=0.25)
+
+        # Move equation up and shrink it
+        self.play(
+            divA.animate.scale(0.5).shift(UP),
+            run_time=2
+        )
+
+        geoEq.next_to(divA, DOWN)
+
+        # Draw the geometric representation of the equation
+        self.play(*[
+            Create(geoEq)
+        ])
+
+class Move(Scene):
+    def construct(self):
+        # f_tex = self.graph_label_tex
+        equation = Tex(
+            "dA", "\\approx", "dx", "x^2",
+            arg_separator=" ",
+            tex_environment="align*"
+        )
+        #equation.to_edge(RIGHT).shift(3*UP)
+        deriv_equation = Tex(
+            "{dA", "\\over \\,", "dx}", "\\approx", "x^2",
+            arg_separator=" ",
+            tex_environment="align*"
+        )
+
+        deriv_equation.move_to(equation, UP+LEFT)
+
+        self.play(*[
+            ReplacementTransform(
+                equation.get_part_by_tex(tex),
+                deriv_equation.get_part_by_tex(tex),
+                run_time = 2,
+            )
+            for tex in ("dA", "approx", "x^2", "dx")
+        ] + [
+            Write(deriv_equation.get_part_by_tex("over"))
+        ])
